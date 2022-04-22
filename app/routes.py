c5c637359a95d31f5b711dba957f8d8bbd550803
Marlyn <marlynlopezz@gmail.com>
2022-04-21 21:21:52 -0700

    JSON response body

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,13 @@ hello_world_bp = Blueprint("hello_world", __name__)
 def say_hello_world():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body, 200 
+
+
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }, 200
