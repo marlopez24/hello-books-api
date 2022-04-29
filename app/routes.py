12931e7c9d863bf0bca93ebee84cd59b63ea8a7f
Marlyn <marlynlopezz@gmail.com>
2022-04-28 22:29:45 -0700

    created a model for books and connected to database with sql

--- app/routes.py
+++ app/routes.py
@@ -1,50 +1,55 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Animal Farm", "Description A"),
-    Book(2, "The Stranger", "Description B"),
-    Book(3, "Persepolis", "Description C"),
-]
+# books = [
+#     Book(1, "Animal Farm", "Description A"),
+#     Book(2, "The Stranger", "Description B"),
+#     Book(3, "Persepolis", "Description C"),
+# ]
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
 books_bp = Blueprint("books", __name__,url_prefix="/books")
 
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
+#     for book in books:
+#         if book.id == book_id:
+#             return book
 
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id" : book.id,
-            "title" : book.title,
-            "description" : book.description
+#     abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id" : book.id,
+#             "title" : book.title,
+#             "description" : book.description
             
-        })
-    return jsonify(books_response), 200
+#         })
+#     return jsonify(books_response), 200
 
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        return {"message":f"book {book_id} invalid"}, 400
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "decsription": book.description
-            }
-        if book.id != book_id: # I also dont have to use this. Just using the return statment where the for loop starts
-            return {"message":f"book {book_id} not found"}, 404
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def handle_book(book_id):
+#     book = validate_book(book_id)
+
+#     return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "decsription": book.description
+#             }
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
