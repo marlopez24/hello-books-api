02479523247ec694f909492b73b01f15adc30613
Marlyn <marlynlopezz@gmail.com>
2022-05-04 23:23:57 -0700

    create two saved books function to test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,10 @@ def test_get_all_books_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_book_by_id(client, two_saved_books):
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+
+
