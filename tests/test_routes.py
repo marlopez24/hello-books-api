20fdce464a9be2154f45476a66bbe8566e709d3b
Marlyn <marlynlopezz@gmail.com>
2022-05-04 23:31:25 -0700

    tested book

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,5 +11,12 @@ def test_get_book_by_id(client, two_saved_books):
     response = client.get("/books/1")
     response_body = response.get_json()
 
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+
 
 
