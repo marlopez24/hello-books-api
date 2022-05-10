4e5ae20fa50df92aba63430c829719aea664713b
Marlyn <marlynlopezz@gmail.com>
2022-05-10 10:38:53 -0700

    updated

--- app/models/author.py
+++ app/models/author.py
@@ -3,4 +3,6 @@ from app import db
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    books = db.relationship("Book", back_populates="author")
+    books = db.relationship("Book", back_populates="author") #list of books
+
+    #parent model 
